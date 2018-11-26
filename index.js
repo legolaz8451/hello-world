729b3909403f17a423d3e51787c0f927cc5ef91c
eduardob <eduardo.buitrago@friendsurance.de>
2018-11-26 11:30:43 +0100

    changing export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function tiny(string) {
+export = function tiny(stringi) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
   return string.replace(/\s/g, "");
 };
